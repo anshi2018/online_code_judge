0004f60e634f4b3aa344a4513e3cb34138bd50c1
Anshi Agarwal <anshimnnit18@gmail.com>
2017-12-17 14:45:27 +0530

    Add files via upload

---
+++ OnlineClient.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onlineclient;
+
+/**
+ *
+ * @author DILIP JI
+ */
+public class OnlineClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new connect().setVisible(true);
+       // Mainserver obj=new Mainserver();
+    }
+}
---
+++ OnlineJudge1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onlinejudge1;
+
+/**
+ *
+ * @author DILIP JI
+ */
+public class OnlineJudge1 {
+
+    /**
+     * @param args the command line arguments
+     * 
+     */
+    
+  /*  public OnlineJudge1()
+    {
+    // initComponents();  
+        initComponents();
+    }*/
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        new Mainserver().setVisible(true);
+      
+    }
+}
